6cd289d2d2aaf8ab7facfb4a9af07f98d9a64e7a
MiwelR <rrriosss@gmail.com>
2020-11-18 01:44:07 +0100

    De números romanos a números enteros

---
+++ main.py
@@ -0,0 +1,14 @@
+from numRomanos import *
+
+print(romano_a_entero('M'))
+print(romano_a_entero('D'))
+print(romano_a_entero('C'))
+print(romano_a_entero('L'))
+print(romano_a_entero('X'))
+print(romano_a_entero('V'))
+print(romano_a_entero('I'))
+print(romano_a_entero('MCM'))
+print(romano_a_entero('MMMCMMM'))
+
+print(romano_a_entero('Z'))
+print(romano_a_entero(23))
